7c038a9f32246a0a1968a95ae468c966a9c214dc
tysek <tysek2004@gmail.com>
2025-06-23 18:57:02 +0200

    Add support for local images

--- src/GUI/ImageCache.py
+++ src/GUI/ImageCache.py
@@ -12,11 +12,15 @@ class ImageCache:
 
         for file in os.listdir(cls.cache_path):
             if str(checksum) == file[:file.index('.')]:
-                with open(cls.cache_path / Path(file), mode='rb') as file:
-                    return file.read()
+                with open(cls.cache_path / Path(file), mode='rb') as opened_file:
+                    return opened_file.read()
 
-        print(f'Fetching profile pic from {link}...')
-        request = urllib.request.urlretrieve(link, cls.cache_path / Path(f'{checksum}.jpg'))
+        try:
+            print(f'Fetching profile pic from {link}...')
+            request = urllib.request.urlretrieve(link, cls.cache_path / Path(f'{checksum}.jpg'))
+        except ValueError:
+            with open(Path(link), mode='rb') as file:
+                return file.read()
 
         with open(cls.cache_path / Path(f'{checksum}.jpg'), mode='rb') as file:
             return file.read()
